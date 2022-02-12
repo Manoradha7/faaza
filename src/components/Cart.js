c67c5f505d71ad96d1e690cb22ddd68c5673e005
Manoradha7 <manoradha7@gmail.com>
2022-02-12 20:12:37 +0530

    commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import '../styles/Cart.css'
+import "../styles/Cart.css";
 import { useState, useEffect } from "react";
 import { API_URL } from "../globalConstant.js";
 
@@ -22,21 +22,30 @@ function Cart() {
   return (
     <div className="cart-container">
       <div className="cart-products-container">
-        <div className='cart-product-title'>
+        <div className="cart-product-title">
           <span>PRODUCT</span>
           <span>PRICE</span>
           <span>QUANTITY</span>
           <span>TOTAL</span>
-        </div><hr/>
-        <div className='cart-product'>
-          <div className='order-product'>
-            <img src="" alt="" className="cart-product-img" />
-            <p>Name</p>
-          </div>
-          <span>₹100</span>
-          <span>Btn</span>
-          <span>total</span>
-        </div><hr/>
+        </div>
+        <hr />
+        {order &&
+          order.map(() => {
+            return (
+              <div>
+                <div className="cart-product">
+                  <div className="order-product">
+                    <img src="" alt="" className="cart-product-img" />
+                    <p>Name</p>
+                  </div>
+                  <span>₹100</span>
+                  <span>Btn</span>
+                  <span>total</span>
+                </div>
+                <hr />
+              </div>
+            );
+          })}
       </div>
       <div className="cart-purchase-detail">
         <p>Cart Total</p>
@@ -44,7 +53,10 @@ function Cart() {
           <p>Subtotal :₹100 </p>
         </div>
         <div>
-          <p>Shipping :There are no shipping methods available. Please double check your address, or contact us if you need any help.</p>
+          <p>
+            Shipping :There are no shipping methods available. Please double
+            check your address, or contact us if you need any help.
+          </p>
           <div></div>
         </div>
       </div>
